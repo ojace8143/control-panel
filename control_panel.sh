f7d601a314337d2928e12816af39c37ea1811bc5
Jace Orcutt <ojace8143@gmail.com>
2026-06-09 17:10:15 -0400

    created brightness subscript and changed main file

--- control_panel.sh
+++ control_panel.sh
@@ -22,63 +22,17 @@ do
     choice=$(gum choose \
         "Volume" \
         "Brightness" \
-	"Power" \
+    	"Power" \
         "System Info" \
         "Quit")
 
     case "$choice" in
-        "Volume")
-	source ~/Scripts/control_panel/subscripts/volume.sh
-	    ;;
-        "Brightness")
-		option=$(gum choose \
-			"Brightness +10%" \
-			"Brightness -10%" \
-			"Max Brightness" \
-			"Set Brightness"  )
-
-		case "$option" in
-			"Brightness +10%")
-				if gum confirm "Increase brightness by 10%?"
-					then
-					brightnessctl set +10%
-				fi
-
-				wait 0.2
-
-				;;
-			"Brightness -10%")
-				if gum confirm "Decrease brightness by 10%?"
-					then
-					brightnessctl set 10%-
-				fi
-				
-				wait 0.2
-
-				;;
-			"Max Brightness")
-				if gum confirm "Set brightness to max?"
-					then
-					brightnessctl set 100%
-				fi
-				
-				wait 0.2
-
-				;;
-			"Set Brightness")
-				num=$(gum input --placeholder "Brightness Percent")
-
-				if gum confirm "Set brightness to ${num}%?"
-					then
-					brightnessctl set ${num}%
-				fi
-
-				wait 0.2
-
-				;;
-
-		esac
-            ;;
+    "Volume")
+	    source ~/Scripts/control_panel/subscripts/volume.sh
+	;;
+    "Brightness")
+        source ~/Scripts/control_panel/subscripts/brightness.sh
+    ;;
 	"Power")
 		option=$(gum choose \
 			"" \
---
+++ subscripts/brightness.sh
@@ -0,0 +1,47 @@
+option=$(gum choose \
+        "Brightness +10%" \
+		"Brightness -10%" \
+		"Max Brightness" \
+		"Set Brightness"  )
+
+case "$option" in
+		"Brightness +10%")
+		if gum confirm "Increase brightness by 10%?"
+			then
+			brightnessctl set +10%
+		fi
+
+		wait 0.2
+
+		;;
+	    "Brightness -10%")
+			if gum confirm "Decrease brightness by 10%?"
+				then
+				brightnessctl set 10%-
+			fi
+				
+			wait 0.2
+
+		;;
+		"Max Brightness")
+			if gum confirm "Set brightness to max?"
+				then
+				brightnessctl set 100%
+			fi
+				
+			wait 0.2
+
+		;;
+		"Set Brightness")
+			num=$(gum input --placeholder "Brightness Percent")
+
+			if gum confirm "Set brightness to ${num}%?"
+				then
+				brightnessctl set ${num}%
+			fi
+
+			wait 0.2
+
+			;;
+esac
+
